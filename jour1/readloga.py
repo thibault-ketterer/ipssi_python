7a98fa057c8aa3a9c47b177f8b909f6f63fe5b28
thibault ketterer <thekit@gmail.com>
2019-11-07 09:36:02 +0100

    date parsing

--- jour1/readloga.py
+++ jour1/readloga.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from datetime import datetime
+
 import os
 
 apath = os.path.expanduser("~/a.log")
@@ -13,7 +15,16 @@ with open(apath) as fd:
         url, httptype, status, size, referer, \
         uagenttrash = line.split(None, 11)
         print(ip)
-        print(date)
+        # print(date)
+        # print(date[1:])
+        # exemple de stack overflow
+        # datetime_object = datetime.strptime('Jun 1 2005  1:33PM',
+        #                                     '%b %d %Y %I:%M%p')
+        # datetime_object = datetime.strptime('29/Oct/2019:06:25:25',
+        #                                     '%d/%b/%Y:%H:%M:%S')
+        datetime_object = datetime.strptime(date[1:],
+                                            '%d/%b/%Y:%H:%M:%S')
+        print(datetime_object)
         print(uagenttrash)
         break
 
