b2b85d6ba5ee2a92381f01f7e21c5d2f3e43e47a
thibault ketterer <thekit@gmail.com>
2019-11-12 09:54:07 +0100

    100% ok exam2

--- exam2/ex04/logs.py
+++ exam2/ex04/logs.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 
 def logthis(msg):
-    open("python.log", "a+").write(str(datetime.now().split(".")[0]) + msg + "\n")
+    open("python.log", "a+").write(str(datetime.now()).split(".")[0] + msg + "\n")
